fc6f75569fd3dc76a3fb39f208698c74f7a4cd81
beta0022 <114179647+beta0022@users.noreply.github.com>
2022-12-28 10:19:58 +0200

    basic

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -4,13 +4,13 @@ export function AppHeader() {
 
     return <header className="app-header">
         <Link to="/">
-            <h3>LOGO!</h3>
+            <img src="assets/img/mario-yoshi-ani.gif" alt="yoshi logo" />
         </Link>
         <nav>
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
             <NavLink to="/mail">Mail</NavLink>
             <NavLink to="/note">Note</NavLink>
+            <NavLink to="/about">About</NavLink>
         </nav>
     </header>
 }
