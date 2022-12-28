85963abd7c7f9cea8e3cdf35fc92def859d8af27
Drorka~ <drorpa4@gmail.com>
2022-12-28 10:40:46 +0200

    update logo and css helpers

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,16 +1,17 @@
 const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
-
-    return <header className="app-header">
-        <Link to="/">
-            <img src="assets/img/mario-yoshi-ani.gif" alt="yoshi logo" />
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-            <NavLink to="/about">About</NavLink>
-        </nav>
-    </header>
+	return (
+		<header className="app-header">
+			<Link to="/">
+				<img src="/assets/img/mario-yoshi-ani.gif" alt="yoshi logo" />
+			</Link>
+			<nav>
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/mail">Mail</NavLink>
+				<NavLink to="/note">Note</NavLink>
+				<NavLink to="/about">About</NavLink>
+			</nav>
+		</header>
+	)
 }
