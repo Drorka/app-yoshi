a62d676c5f0e55aff37d734a346b5a80819f8095
Drorka~ <drorpa4@gmail.com>
2022-12-28 10:21:38 +0200

    create basic routing

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -7,19 +7,27 @@ import { Home } from './views/home.jsx'
 import { About } from './views/about.jsx'
 import { MailIndex } from './apps/mail/views/mail-index.jsx'
 import { NoteIndex } from './apps/note/views/note-index.jsx'
-
-
+import { MailDetails } from './apps/mail/views/mail-details.jsx'
+import { NoteDetails } from './apps/note/views/note-details.jsx'
+import { MailEdit } from './apps/mail/views/mail-edit.jsx'
+import { NoteEdit } from './apps/note/views/note-edit.jsx'
 
 export function App() {
-    return <Router>
-        <section className="app">
-            <AppHeader />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/mail" element={<MailIndex />} />
-                <Route path="/note" element={<NoteIndex />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </section>
-    </Router>
+	return (
+		<Router>
+			<section className="app">
+				<AppHeader />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/mail" element={<MailIndex />} />
+					<Route path="/mail/:mailId" element={<MailDetails />} />
+					<Route path="/mail/edit/:mailId" element={<MailEdit />} />
+					<Route path="/note" element={<NoteIndex />} />
+					<Route path="/note/:noteId" element={<NoteDetails />} />
+					<Route path="/note/edit/:noteId" element={<NoteEdit />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</section>
+		</Router>
+	)
 }
