e33401bbe341a98816b6d2583b84c889558dc6d1
Drorka~ <drorpa4@gmail.com>
2022-12-28 15:11:56 +0200

    render mail preview

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,8 +1,16 @@
 export function MailPreview({ mail }) {
 	return (
-		<article className="mail-preview">
-			<span className="mail-subject">{mail.subject}</span>
-			<span className="mail-body">{mail.body}</span>
+		<article className="mail-preview flex">
+			<div className="mail-markers">
+				<button className="btn-mail-marker">&#9744;</button>
+				<button className="btn-mail-marker">&#9734;</button>
+				<button className="btn-mail-marker">&#9750;</button>
+			</div>
+			<div className="mail-sender">{mail.from}</div>
+			<div className="mail-subject">{mail.subject}</div>
+			<div className="mail-body">{mail.body}</div>
+			<div className="mail-date">{mail.sentAt}</div>
+			<div className="mail-quick-crudl"></div>
 		</article>
 	)
 }
