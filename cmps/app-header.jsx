25905dd2d25ddc95792929e19375fdf544f96a78
Drorka~ <drorpa4@gmail.com>
2022-12-28 21:20:02 +0200

    header icon debug

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -1,14 +1,15 @@
 const { Link, NavLink } = ReactRouterDOM
+import { home_logo } from '../assets/img/icons/home_logo.png'
 
 export function AppHeader() {
 	return (
 		<header className="app-header">
 			<Link to="/">
-				<img src="/../assets/img/mario-yoshi-ani.gif" alt="yoshi logo" />
+				<img src="../assets/img/mario-yoshi-ani.gif" alt="yoshi logo" />
 			</Link>
 			<nav>
 				<NavLink to="/">
-					<img src="../assets/img/icons/home_logo.png" alt="home" />
+					<img src={home_logo} alt="home" />
 				</NavLink>
 
 				<NavLink to="/mail">
