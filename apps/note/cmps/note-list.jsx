7f5a24e9ff2890f04040dc2844d854fc99ff14bd
beta0022 <114179647+beta0022@users.noreply.github.com>
2022-12-29 12:45:54 +0200

    delete notes comp

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -3,14 +3,14 @@ import { noteService } from '../services/note.service.js'
 import { NotePreview } from './note-preview.jsx'
 
 
-export function NoteList( { notes } ) {
+export function NoteList( { notes, deleteNote } ) {
 
     return <section className="note-list">
 
             {
             notes.map((note) => (
 				<div key={note.id}>
-					<NotePreview note={note} />
+					<NotePreview note={note} deleteNote={deleteNote} />
 				</div>
 			))
             }
