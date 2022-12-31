5cf51a22c7fbaa280aa5a18b3acf0d037716ba91
beta0022 <114179647+beta0022@users.noreply.github.com>
2022-12-31 18:51:20 +0200

    Update app-header.jsx

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -4,23 +4,23 @@ export function AppHeader() {
 	return (
 		<header className="app-header">
 			<Link to="/">
-				<img src="assets/img/mario-yoshi-ani.gif" alt="yoshi logo" />
+				<img src="../assets/img/mario-yoshi-ani.gif" alt="yoshi logo" />
 			</Link>
 			<nav>
 				<NavLink to="/">
-					<img src="assets/img/icons/home_logo.png" alt="home" />
+					<img src="../assets/img/icons/home_logo.png" alt="home" />
 				</NavLink>
 
 				<NavLink to="/mail">
-					<img src="assets/img/icons/mail_logo.png" alt="mail" />
+					<img src="../assets/img/icons/mail_logo.png" alt="mail" />
 				</NavLink>
 
 				<NavLink to="/note">
-					<img src="assets/img/icons/keep_logo.png" alt="note" />
+					<img src="../assets/img/icons/keep_logo.png" alt="note" />
 				</NavLink>
 
 				<NavLink to="/about">
-					<img src="/assets/img/icons/contacs_logo.png" alt="about" />
+					<img src="../assets/img/icons/contacs_logo.png" alt="about" />
 				</NavLink>
 			</nav>
 		</header>
