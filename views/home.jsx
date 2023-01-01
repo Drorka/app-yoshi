12c3906ad8bdeee1fc815ed631b15de933ecd02a
Drorka~ <drorpa4@gmail.com>
2023-01-01 08:45:13 +0200

    capitalize homepage

--- views/home.jsx
+++ views/home.jsx
@@ -1,12 +1,11 @@
-
 export function Home() {
+	return (
+		<section className="home">
+			<h1>Yoshi is the new Appsus</h1>
+			<h2>Helps simplify your life</h2>
+			<h4>*and save the princess</h4>
 
-    return <section className="home">
-        <h1>Yoshi is the new Appsus</h1>
-        <h2>helps to simplify your life</h2>
-        <h4>*and save the princess</h4>
-
-        <img src="assets/img/yoshi.png" alt="yoshi" />
-
-    </section>
+			<img src="assets/img/yoshi.png" alt="yoshi" />
+		</section>
+	)
 }
